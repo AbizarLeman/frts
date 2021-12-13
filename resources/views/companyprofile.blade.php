4ed4926e952feb64b53a6cbf0032e0d5842fbe48
Arni Rani <95959758+Me1ody0@users.noreply.github.com>
2021-12-13 17:46:03 +0800

    Rearrange the profile
    percubaan

--- resources/views/companyprofile.blade.php
+++ resources/views/companyprofile.blade.php
@@ -2,44 +2,34 @@
     <thead class="thead-light">
         <tr>
             <th scope="col">Company ID</th>
-            <th scope="col">Company Name</th>
-            <th scope="col">ROCBN</th>
-            <th scope="col">District</th>
-            <th scope="col">Mukim</th>
-            <th scope="col">Village</th>
-            <th scope="col">Street Address</th>
-            <th scope="col">Rice</th>
-            <th scope="col">Broiler</th>
-            <th scope="col">Vegetables</th>
-            <th scope="col">Fruits</th>
-            <th scope="col">Buffaloes</th>
-            <th scope="col">Cattle</th>
-            <th scope="col">Goats</th>
-            <th scope="col">Cut Flowers</th>
-            <th scope="col">Egg</th>
-            <th scope="col">Ornamental Horticulture</th>
-            <th scope="col">Miscellaneous</th>
+            <th scope="col">Company Description</th>
+            <th scope="col">Type of produce list</th>
             <th scope="col">Action</th>
         </tr>
     </thead>
     <tbody>
         <tr>
             <td>{{ $company->id }}</td>
-            <td>{{ $company->company_name }}</td>
-            <td>{{ $company->rocbn }}</td>
-            <td>{{ $company->district }}</td>
-            <td>{{ $company->mukim }}</td>
-            <td>{{ $company->village }}</td>
-            <td>{{ $company->street_address }}</td>
-            <td>{{ $company->rice }}</td>
-            <td>{{ $company->broiler }}</td>
-            <td>{{ $company->vegetable }}</td>
-            <td>{{ $company->fruit }}</td>
-            <td>{{ $company->buffalo }}</td>
-            <td>{{ $company->cattle }}</td>
-            <td>{{ $company->egg }}</td>
-            <td>{{ $company->ornamental_horticulture }}</td>
-            <td>{{ $company->miscellaneous_string }}</td>
+
+            <td><b>Company Name:</b>{{ $company->company_name }}<br>
+                <b>ROCBN:</b>{{ $company->rocbn }}<br>
+                <b>District:</b>{{ $company->district }}<br>
+                <b>Mukim:</b>{{ $company->mukim }}<br>
+                <b>Village:</b>{{ $company->village }}<br>
+                <b>Street Address:</b>{{ $company->street_address }}<br>
+            </td>
+
+            <td><b>Rice:</b>{{ $company->rice }}<br>
+                <b>Broiler:</b>{{ $company->broiler }}<br>
+                <b>Vegetable:</b>{{ $company->vegetable }}<br>
+                <b>Fruit:</b>{{ $company->fruit }}<br>
+                <b>Buffalo:</b>{{ $company->buffalo }}<br>
+                <b>Cattle:</b>{{ $company->cattle }}<br>
+                <b>Egg:</b>{{ $company->egg }}</td>
+                <b>Ornamental Horticulture:</b>{{ $company->ornamental_horticulture }}<br>
+                <b>Miscellaneous</b>{{ $company->miscellaneous_string }}<br>
+            </td>
+
             <td>
                 {{-- <a href="{{ url('/company/edit/'.$employee->id) }}" class="btn btn-sm btn-warning">Edit</a>
                 <a href="{{ url('/company/destroy/'.$employee->id) }}" class="btn btn-sm btn-danger">Delete</a> --}}
