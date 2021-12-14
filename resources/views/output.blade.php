997dd614e33b6b8cd55f3926108667103887807e
AbizarLeman <56353860+AbizarLeman@users.noreply.github.com>
2021-12-14 11:05:10 +0800

    Extracted the rice output form into addrice file.

--- resources/views/output.blade.php
+++ resources/views/output.blade.php
@@ -45,37 +45,7 @@
                         </div>
                         @if ($type == 'rice')
 
-                        <div class="form-group row">
-                            <label for="planted-at" class="col-md-4 col-form-label text-md-right">Planting Date</label>
-
-                            <div class="col-md-6">
-                                <input id="planted-at" type="date" class="form-control" name="planted-at" required autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="packaged-at" class="col-md-4 col-form-label text-md-right">Harvesting/Packaging Date</label>
-
-                            <div class="col-md-6">
-                                <input id="packaged-at" type="date" class="form-control" name="packaged-at" required autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="quantity-packaged" class="col-md-4 col-form-label text-md-right">Quantity Packaged</label>
-
-                            <div class="col-md-6">
-                                <input id="quantity-packaged" type="number" class="form-control" name="quantity-packaged" required autofocus>
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="kg-per-packaging" class="col-md-4 col-form-label text-md-right">Kilogram per Packaging</label>
-
-                            <div class="col-md-6">
-                                <input id="kg-per-packaging" type="number" step="any" class="form-control" name="kg-per-packaging" required autofocus>
-                            </div>
-                        </div>
+                        @include('addrice')
                             
                         @endif
                         <div class="form-group row">
