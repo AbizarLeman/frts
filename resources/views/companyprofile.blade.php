70a1dbd3fe8e97b2ed51ce21f5d7f1eec06d56cd
hadizulkiflee <95990069+hadizulkiflee@users.noreply.github.com>
2021-12-15 17:03:24 +0800

    Revert "Merge branch 'master' of https://github.com/AbizarLeman/frts"
    This reverts commit 8029b2fd2f86658b86a358e0546e2f336fe370fe, reversing
    changes made to 83d48ce6d8c601a7d75d95163dd19a3c53ea8999.

--- resources/views/companyprofile.blade.php
+++ resources/views/companyprofile.blade.php
@@ -3,7 +3,7 @@
         <tr>
             <th scope="col">Company ID</th>
             <th scope="col">Company Description</th>
-            <th scope="col">Agriculture Output Type</th>
+            <th scope="col">Type of produce list</th>
             <th scope="col">Action</th>
         </tr>
     </thead>
@@ -11,31 +11,30 @@
         <tr>
             <td>{{ $company->id }}</td>
 
-            <td><b>Company Name: </b>{{ $company->company_name }}<br>
-                <b>ROCBN: </b>{{ $company->rocbn }}<br>
-                <b>District: </b>{{ $company->district }}<br>
-                <b>Mukim: </b>{{ $company->mukim }}<br>
-                <b>Village: </b>{{ $company->village }}<br>
-                <b>Street Address: </b>{{ $company->street_address }}<br>
+            <td><b>Company Name:</b>{{ $company->company_name }}<br>
+                <b>ROCBN:</b>{{ $company->rocbn }}<br>
+                <b>District:</b>{{ $company->district }}<br>
+                <b>Mukim:</b>{{ $company->mukim }}<br>
+                <b>Village:</b>{{ $company->village }}<br>
+                <b>Street Address:</b>{{ $company->street_address }}<br>
             </td>
 
-            <td><b>Rice: </b>{{ $company->rice }}<br>
-                <b>Broiler: </b>{{ $company->broiler }}<br>
-                <b>Vegetable: </b>{{ $company->vegetable }}<br>
-                <b>Fruit: </b>{{ $company->fruit }}<br>
-                <b>Buffalo: </b>{{ $company->buffalo }}<br>
-                <b>Cattle: </b>{{ $company->cattle }}<br>
-                <b>Cut Flower: </b>{{ $company->cattle }}<br>
-                <b>Egg: </b>{{ $company->egg }}<br>
-                <b>Ornamental Horticulture: </b>{{ $company->ornamental_horticulture }}<br>
-                <b>Miscellaneous: </b>{{ $company->miscellaneous_string }}<br>
+            <td><b>Rice:</b>{{ $company->rice }}<br>
+                <b>Broiler:</b>{{ $company->broiler }}<br>
+                <b>Vegetable:</b>{{ $company->vegetable }}<br>
+                <b>Fruit:</b>{{ $company->fruit }}<br>
+                <b>Buffalo:</b>{{ $company->buffalo }}<br>
+                <b>Cattle:</b>{{ $company->cattle }}<br>
+                <b>Egg:</b>{{ $company->egg }}<br>
+                <b>Ornamental Horticulture:</b>{{ $company->ornamental_horticulture }}<br>
+                <b>Miscellaneous:</b>{{ $company->miscellaneous_string }}<br>
             </td>
 
             <td>
                 {{-- <a href="{{ url('/company/edit/'.$employee->id) }}" class="btn btn-sm btn-warning">Edit</a>
                 <a href="{{ url('/company/destroy/'.$employee->id) }}" class="btn btn-sm btn-danger">Delete</a> --}}
-                <a href="{{url ('/company/edit/'.$company->id)}}" class="btn btn-sm btn-warning">Edit</a>
-                <a href="{{url ('/company/destroy/'.$company->id)}}" class="btn btn-sm btn-danger">Delete</a>
+                <a href="#" class="btn btn-sm btn-warning">Edit</a>
+                <a href="#" class="btn btn-sm btn-danger">Delete</a>
             </td>
         </tr>     
     </tbody>
